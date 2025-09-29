5463a84f1c1d9882c011fc7233f2f898b49a61bb
B00164511 Cianan Carey <B00164511@mytudublin.ie>
2025-09-29 15:53:25 +0100

    Step 1

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import {View, Text, TextInput} from 'react-native';
 import React, {useState} from 'react';
-
+import Logo from "./components/Logo";
 
 export default function App(){
-  const [fullname, setFullname] = useState("Eoin");
+  const [fullname, setFullname] = useState("Cianán");
   return(
     <View> 
+      <Logo/>
       <Text>Hello, World {fullname}</Text>
       <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
     </View>
   )
 };
-
-
 
