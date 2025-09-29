09b6863fce95e5bfab52000cd50f3f51438ffd3b
B00164511 Cianan Carey <B00164511@mytudublin.ie>
2025-09-29 16:49:07 +0100

    Step 3

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, Button, Alert} from 'react-native';
 import React, {useState} from 'react';
 import Logo from "./components/Logo";
 
 export default function App(){
-  const [fullname, setFullname] = useState("Cianán");
-  const [fname, setFname] = useState("Joe");
-  const [lname, setLname] = useState("Bloggs");
-  const [dob, setDob] = useState("22 August 1990");
+  const [fullname, setFullname] = useState("Cianán Carey");
+  const [fname, setFname] = useState("Cianán");
+  const [lname, setLname] = useState("Carey");
+  const [dob, setDob] = useState("19 November 2004");
+  
+  function buttonClicked() {
+    //Alert.alert("Hello {fname} {lname}, your date of birth is {dob}"); //This works on a mobile phone
+    alert("Hello " + fname + lname + ". Your Date of Birth is " + dob); // this works on the web version - try uncommenting one or the other lines as necessary
+}
   
   return(
     <View> 
@@ -16,7 +21,9 @@ export default function App(){
       <TextInput placeholder="Enter your firstname" onChangeText={setFname}/>
       <TextInput placeholder="Enter your lastname" onChangeText={setLname}/>
       <TextInput placeholder="Enter your date of birth" onChangeText={setDob}/>
+      <Button title="SUBMIT" onPress={buttonClicked}/>
       <Text>Hello {fname} {lname}. You were born on {dob}</Text>
+      
     </View>
   )
 };
